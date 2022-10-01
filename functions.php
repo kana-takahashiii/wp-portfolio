276dcab465f3e3148f1e8f8d93c5f4a277120cf9
kana-takahashiii <kana92105@gmial.com>
2022-10-01 17:44:26 +0900

    アイキャッチ画像をcssで調整できるようにする

--- functions.php
+++ functions.php
@@ -8,3 +8,13 @@ add_theme_support( 'title-tag' );
  * アイキャッチ画像を使用可能にする
  */
 add_theme_support('post-thumbnails');
+
+/**
+ * アイキャッチ画像をcssで調整できるようにする
+ */
+add_filter( 'post_thumbnail_html', 'custom_attribute2' );
+function custom_attribute2( $html ){
+    // width height を削除する
+    $html = preg_replace('/(width|height)="\d*"\s/', '', $html);
+    return $html;
+}
