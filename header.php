6302191e8b3a8a4563f83faef0642755425a2695
kana-takahashiii <kana92105@gmial.com>
2022-10-01 11:36:55 +0900

    header>タイトル変更

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?php bloginfo('name'); ?></title>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
         <!-- OG画像 -->
         <meta property="og:title" content="KANA TAKAHASHI Portfolio">
