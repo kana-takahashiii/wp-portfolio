4a3e68d45abcb2344f167a5422d5c5779e2c175d
kana-takahashiii <kana92105@gmial.com>
2022-10-06 22:00:58 +0900

    no commit

--- header.php
+++ header.php
@@ -17,9 +17,7 @@
         <meta property="og:site_name" content="KANA TAKAHASHI">
 
         <!-- ファビコン -->
-        <!-- <link rel="shortcut icon" href="https://www.kana-takahashi.com/img/favicon(32).ico" type="image/vnd.microsoft.icon">
-        <link rel="icon" href="https://www.kana-takahashi.com/img/favicon(32).ico" type="image/vnd.microsoft.icon">   -->
-        <link rel="shortcut icon" href="./img/favicon.ico">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>./img/favicon.ico">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" href="/android-chrome-192x192.png">
                 
