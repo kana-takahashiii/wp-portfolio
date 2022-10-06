9233f0de66b85d92b8a2d54b0118797ea0e444fe
kana-takahashiii <kana92105@gmial.com>
2022-10-06 12:53:48 +0900

    テキストエリアの幅調整

--- page-thanks.php
+++ page-thanks.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
+
 <div class="contact-thanks">
+    <div class="section-wrap js-trigger" id="confirm">
+        <h2 class="js-span">THANKS!</h2>
+    </div>
     <?php the_content(); ?>
 </div>
+
 
 <?php get_footer(); ?>
