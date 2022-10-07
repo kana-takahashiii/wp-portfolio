2fc285dbd9007d1dc2e20089f0fff99c2a43e76f
kana-takahashiii <kana92105@gmial.com>
2022-10-07 16:32:50 +0900

    404 class名変更

--- 404.php
+++ 404.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
 
-<h2 class="page-title">404 NOT FOUND<span>ERROR</span></h2>
 
-<div class="main">
-    <div class="container">
+    <div class="container-404">
+        <h2 class="error">404 NOT FOUND ERROR</h2>
         <p>お探しのページが見つかりませんでした。</p>
         <p>申し訳ございませんが、<a href="<?php echo home_url('/'); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
     </div>
-</div>
 
 <?php get_footer(); ?>
